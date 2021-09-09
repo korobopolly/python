e7e0fa9031c08286e79c9892232bb8e1572f911f
raininnight <shqkd1492@gmail.com>
2021-09-09 10:59:39 +0900

    파이썬 입문

---
+++ string.py
@@ -0,0 +1,35 @@
+print("Hello, world!")
+print('Hello, world!')
+
+print("ABC")
+print("A'sC")
+print('ABC')
+print('A"sC')
+print("""ABC""")
+print('''ABC''')
+
+print("""Hello, world!
+안녕하세요.
+Python 입니다.""")
+print("안녕"+" "+"파이썬")
+
+print("10"+"20")
+print(10+20)
+
+#반복 출력
+print("파이썬 "*10)
+print("파이썬 "*-1)
+
+#숫자 출력
+print(1)
+print(1, 2)
+print("a","b")
+print("a",10)
+
+#sys 모듈
+import sys
+print(sys.maxsize)
+print(-sys.maxsize-1)
+print(12345678901234567890)
+print(-12345678901234567890)
+print(0.12345678901234567890)

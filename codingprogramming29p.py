18316339243bf945e9f6dbafde19f00c8429e163
raininnight <shqkd1492@gmail.com>
2021-09-17 14:36:30 +0900

    제출과제(실기)

---
+++ codingprogramming29p.py
@@ -0,0 +1,57 @@
+#1번 문제
+print("20161449", "이준성")
+print("20161449"+ "이준성")
+
+#2번 문제
+print(("20161449"+"이준성")*3)
+
+#3번 문제
+print(3,-10,0)
+print(3.14,-10.01,0.0)
+
+#4번 문제
+print(4+3*5)
+print((4+3)*5)
+print(4+(3*5))
+print((4+3*5))
+print(8/2)
+print(8/-2)
+print(-8/2)
+print(-8/-2)
+
+#5번 문제
+print("1+3+5+7+9=",1+3+5+7+9)
+
+#6번 문제
+import turtle as t
+t.shape("turtle")
+t.forward(100)
+
+t.shape("arrow")
+t.left(180)
+t.forward(50)
+
+t.shape("circle")
+t.forward(50)
+
+#7번 문제
+import turtle as t
+t.reset()
+t.shape("turtle")
+t.forward(100)
+
+t.left(120)
+t.forward(100)
+
+t.left(120)
+t.forward(100)
+
+#8번 문제
+t.reset()
+t.shape("turtle")
+t.circle(100)
+
+#9번 문제
+t.shape("turtle")
+t.circle(100,steps=3)
+

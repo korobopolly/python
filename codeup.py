cacea71da22fb9ee4d760a8e4b0b3688c7b58b64
raininnight <shqkd1492@gmail.com>
2021-11-07 02:59:15 +0900

    Python 기초 100제 1~8

---
+++ codeup.py
@@ -0,0 +1,24 @@
+#1번 문제
+print("Hello")
+
+#2번 문제
+print("Hello","World")
+
+#3번 문제
+print("Hello")
+print("World")
+
+#4번 문제
+print("\'Hello\'")
+
+#5번 문제
+print("\"Hello World\"")
+
+#6번 문제
+print("\"!@#$%^&*()\'")
+
+#7번 문제
+print("\"C:\\Download\\\'hello\'.py\"")
+
+#8번 문제
+print("print(\"Hello\\nWorld\")")

4fa63462b7afca22c383484bf0b00cf6a640530f
raininnight <shqkd1492@gmail.com>
2021-11-08 01:20:45 +0900

    Python 기초 100제 9~18

--- codeup.py
+++ codeup.py
@@ -1,24 +1,72 @@
-#1번 문제
+'''
+#6001번 문제
 print("Hello")
 
-#2번 문제
+#6002번 문제
 print("Hello","World")
 
-#3번 문제
+#6003번 문제
 print("Hello")
 print("World")
 
-#4번 문제
+#6004번 문제
 print("\'Hello\'")
 
-#5번 문제
+#6005번 문제
 print("\"Hello World\"")
 
-#6번 문제
+#6006번 문제
 print("\"!@#$%^&*()\'")
 
-#7번 문제
+#6007번 문제
 print("\"C:\\Download\\\'hello\'.py\"")
 
-#8번 문제
+#6008번 문제
 print("print(\"Hello\\nWorld\")")
+
+#6009번 문제
+c=input()
+print(c)
+
+#6010번 문제
+c=int(input())
+print(c)
+
+#6011번 문제
+c=float(input())
+print(c)
+
+#6012번 문제
+a=int(input())
+b=int(input())
+print(a)
+print(b)
+
+#6013번 문제
+a=input()
+b=input()
+#a,b = b,a
+print(b)
+print(a)
+
+#6014번 문제
+f=float(input())
+for i in range(3):
+    print(f)
+
+#6015번 문제
+a,b=input().split() #공백을 기준으로 입력된 값들을 나누어 자름
+print(a)
+print(b)
+
+#6016번 문제
+c1, c2=input().split()
+print(c2,c1)
+
+#6017번 문제
+s=input()
+print(s,s,s)
+'''
+#6018번 문제
+time,minute=input().split(':') #split 문자를 기준으로 자름
+print(time,minute,sep=':') #sep 문자를 사이에 두고 출력

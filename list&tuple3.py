0a1cb1abe5fb529cba84dba765d8f5135af4669f
raininnight <shqkd1492@gmail.com>
2021-11-21 17:52:14 +0900

    12주차 실습 (2)

--- list&tuple3.py
+++ list&tuple3.py
@@ -36,7 +36,7 @@ print(sum(a))
 '''
 #2. 리스트 포함문 사용하기
 #리스트 Comprehension - 리스트 내부에서 for 문과 if 문 사용가능 ( [ ] == list() )
-
+'''
 #1) for 문 포함
 a=[i for i in range(10)]
 print(a)
@@ -52,3 +52,100 @@ print(b)
 #3) for 문을 중복해서 사용
 c=[i*j for i in range(2,10) for j in range(1,10)]
 print(c)
+'''
+#3. 리스트에 map 함수 사용하기
+'''
+a=[1.2,2.5,3.8,4.7]
+for i in range(len(a)):
+    a[i]=int(a[i])
+print(a)
+
+a=[1.2,2.5,3.8,4.7]
+a=list(map(int,a))
+print(a)
+
+a=[1.2,2.5,3.8,4.7]
+a=[map(int,a)] #함수는 함수 안에서 써야한다.
+print(a)
+
+a=[1.2,2.5,3.8,4.7]
+a=map(int,a)
+print(list(a))
+
+b=list(map(float,range(10)))
+print(b)
+
+c=input().split()
+print(c)
+
+d=map(int,input().split())
+print(list(d))
+'''
+#4. 튜플에서 메서드 사용
+#튜플에서는 변경, 삭제 등을 제외한 메서드만 가능하다.
+'''
+a=(38,21,38,62,19)
+#a[0] = 50
+print(a.index(62))
+print(a.count(38))
+
+a=(1,2,3)
+b=(10,20,30)
+c=a+b
+print(c)
+d=c*2 #배수만큼 반복 출력
+print(d)
+
+print(30 in d)
+print(40 in d)
+print(40 not in d)
+'''
+#5. 튜플 Comprehansion
+'''
+a=[i for i in range(10)]
+print(a)
+
+a=(i for i in range(10))
+#print(list(a))
+print(tuple(a))
+
+b=tuple(i for i in range(10) if i%2==0)
+print(b)
+'''
+#6. map함수 및 max, min, sum 함수의 사용
+'''
+c=(1.2,2.5,3.4,4.7)
+c=tuple(map(int,c))
+print(c)
+
+print(max(c),
+min(c),
+sum(c),
+sum(c)/len(c))
+'''
+#7. 2차원 리스트의 사용
+'''
+a=[[1,2],[10,20]] #2*2 리스트
+b=[[1,2,3],[10,20,30],[100,200,300]] #3*3 리스트
+
+print(len(a),len(b)) #아래로 쌓이는 세로줄 수
+#인덱스 표현 a[세로 인덱스][가로 인덱스]
+
+print(b[2][2])
+'''
+#8. 2차원 리스트 모든 요소 출력
+
+a=[[10,20],[30,40],[50,60]] #3*2 리스트
+
+for x,y in a:
+    print(x,y)
+
+for i in a:
+    for j in i:
+        print(j,end=" ")
+    print()
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=" ")
+    print()
